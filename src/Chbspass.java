ff17bb8718e64962083afab66da6bcee7a55421b
Noah Holt <Noah.Holt@abcfinancial.com>
2018-06-29 17:01:23 -0500

    Got builder pattern working.

--- src/Chbspass.java
+++ src/Chbspass.java
@@ -3,26 +3,52 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 
 public class Chbspass {
-    private final static String[] words = new String[1000];
-    private int passwordLength;
-    private int numNumbers;
-    private int numSpecial;
-    private int constraintLength;
+    private final static int numWords = 1000;
+    private final static String[] words = new String[numWords];
     private final static String filepath = "C:\\Users\\NHol01\\IdeaProjects\\chbspass\\src\\top-1000-words-uc.txt";
     private final static String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-    private final static  String[] special_chars = {"!", "@", "#", "$", "^", "%", "&", "*"};
+    private final static  String[] symbols = {"!", "@", "#", "$", "^", "%", "&", "*"};
+    private final int passwordLength;
+    private final int numNumbers;
+    private final int numSymbols;
+    private final int constraintLength;
     private SecureRandom PwdGen = new SecureRandom();
     static {
         Chbspass.readWordFile(filepath);
     }
 
-    public Chbspass(int passwordLength, int numNumbers, int numSpecial) {
-        this.passwordLength = passwordLength;
-        this.numNumbers = numNumbers;
-        this.numSpecial = numSpecial;
-        this.constraintLength = this.passwordLength - this.numNumbers - this.numSpecial;
+    public Chbspass(Password password) {
+        this.passwordLength = password.passwordLength;
+        this.numNumbers = password.numNumbers;
+        this.numSymbols = password.numSymbols;
+        this.constraintLength = this.passwordLength - this.numNumbers - this.numSymbols;
+    }
+
+    public static class Password {
+        private int passwordLength;
+        private int numNumbers = 2;
+        private int numSymbols = 2;
+
+        public Password(int passwordLength) {
+            this.passwordLength = passwordLength;
+        }
+
+        public Password numNumbers(int val) {
+            numNumbers = val;
+            return this;
+        }
+
+        public Password numSymbols(int val) {
+            numSymbols = val;
+            return this;
+        }
+
+        public Chbspass generate() {
+            return new Chbspass(this);
+        }
     }
 
     private static void readWordFile(String filepath) {
@@ -41,7 +67,7 @@ public class Chbspass {
         }
     }
 
-    public String generatePassword() {
+    public String Next() {
         StringBuilder pw = new StringBuilder();
         String word = "";
         while (pw.length() < this.constraintLength) {
@@ -51,15 +77,25 @@ public class Chbspass {
             while (word.length() + pw.length() > this.constraintLength);
             pw.append(word);
         }
+
         for (int x = 0; x < this.numNumbers; x++) {
             String number = numbers[PwdGen.nextInt(9)];
             pw.append(number);
         }
-        for (int y = 0; y < this.numSpecial; y++) {
-            String special = special_chars[PwdGen.nextInt(8)];
-            pw.append(special);
+
+        for (int y = 0; y < this.numSymbols; y++) {
+            String symbol = symbols[PwdGen.nextInt(8)];
+            pw.append(symbol);
         }
+
         return pw.toString();
     }
 
+    public ArrayList<String> Next(int n) {
+        ArrayList<String> pws = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            pws.add(this.Next());
+        }
+        return pws;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Chbspass test = new Chbspass(30, 2, 3);
-        String potato = test.generatePassword();
-        System.out.println(potato);
-        System.out.println(potato.length());
+        Chbspass test = new Chbspass.Password(30).numNumbers(2).numSymbols(2).generate();
+        ArrayList<String> pwds = test.Next(10);
+        for (String pwd : pwds) {
+            System.out.println(pwd);
+        }
     }
 }
